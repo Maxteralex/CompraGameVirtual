d244e8efe2e53405446ad852dea23319a9e744d5
Alexandre Heine <xandeaph@gmail.com>
2020-10-30 04:04:37 -0300

    Update CompraGameVirtual.sol

--- CompraGameVirtual.sol
+++ CompraGameVirtual.sol
@@ -7,13 +7,15 @@ contract SellGameItem {
     string item_name; // nome do item vendido
     uint item_id; // id do item vendido
     uint item_price; // preço do item vendido
+    bool item_sold;
     
-    constructor(string memory _owner_name, string memory _item_name, uint _item_id, uint _item_price) public{
+    constructor(string memory _owner_name, string memory _item_name, uint _item_id, uint _item_price) public {
         owner = msg.sender;
         owner_name = _owner_name;
         item_name = _item_name;
         item_id = _item_id;
         item_price = _item_price;
+        item_sold = false;
     }
     
     modifier checkOwnership() {
@@ -22,28 +24,33 @@ contract SellGameItem {
     }
     
     // o dono pode modificar o preço do item do contrato
-    function setItemPrice(uint _item_price) public checkOwnership{
-         item_price = _item_price;
+    function setItemPrice(uint _item_price) public checkOwnership {
+        item_price = _item_price;
+    }
+
+    function sellItem() public checkOwnership {
+        item_sold = false;
     }
     
     // qualquer pessoa pode checar o preço atual do item
-    function getItemPrice() public view returns (uint){
+    function getItemPrice() public view returns (uint) {
         return item_price;
     }
     
-    function getItemName() public view returns (string memory){
+    function getItemName() public view returns (string memory) {
         return item_name;
     }
     
-    function getOwnerName() public view returns (string memory){
+    function getOwnerName() public view returns (string memory) {
         return owner_name;
     }
     
-    function buyItem() payable public{
-        // require (msg.value > item_price, "O valor enviado foi maior que o estipulado pelo dono do contrato.");
+    function buyItem() payable public {
+        require (msg.sender == owner, "Você já é o dono do item.");
+        require (item_sold, "O item já foi vendido.");
         require (msg.value < item_price, "O valor enviado foi menor que o estipulado pelo dono");
         
-        if (msg.value >= item_price){
+        if (msg.value >= item_price) {
             uint change = msg.value - item_price;
             // se o valor pago for maior que o estipulado devolve a quantia excedente
             if (change > 0) {
